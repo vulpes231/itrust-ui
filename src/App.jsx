fe0a7a820ac8c85623ce0b8d3d30a644a290762e
Vulpes231 <whykayshot@gmail.com>
2024-05-29 14:25:28 +0100

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="font-[Montserrat]">Welcome to iTrust Investment</h1>
+      <p className="bebas-neue-regular uppercase">How can i help you today?</p>
+    </div>
+  );
 };
 
 export default App;
