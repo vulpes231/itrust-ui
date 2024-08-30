9bfff67bd0ac3f749da0c93a756711585b6cb7b5
Vulpes231 <whykayshot@gmail.com>
2024-08-30 14:13:53 +0100

    ui

--- src/components/Fundedmodal.jsx
+++ src/components/Fundedmodal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const styles = {
   input: `w-full border border-slate-700 dark:border-slate-200 bg-transparent p-2 outline-none focus:outline-purple-500 focus:border-none`,
@@ -6,8 +6,6 @@ const styles = {
   textarea: `w-full border border-slate-700 dark:border-slate-200 bg-transparent p-2 outline-none focus:outline-purple-500 focus:border-none`,
   label: `capitalize font-medium`,
 };
-
-const accounts = ["Basic IRA", "Offshore account"];
 
 const Fundedmodal = ({ closeModal }) => {
   const [form, setForm] = useState({
@@ -18,6 +16,7 @@ const Fundedmodal = ({ closeModal }) => {
   });
 
   const [error, setError] = useState(false);
+  const modalRef = useRef();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -40,18 +39,37 @@ const Fundedmodal = ({ closeModal }) => {
       amount: "",
     });
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        closeModal();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [closeModal]);
+
   return (
     <div className="w-full h-screen fixed top-0 left-0 flex items-center pb-10 pt-20 bg-black bg-opacity-30">
-      <div className="h-full dark:bg-white bg-black w-full lg:max-w-[32%] lg:mx-auto">
+      <div
+        ref={modalRef}
+        className="h-full dark:bg-white bg-black w-full lg:max-w-[32%] lg:mx-auto"
+      >
         <div>
           <h3 className="p-6 capitalize font-bold text-sm">request funds</h3>
           <hr />
           <form action="" className="flex flex-col gap-4 p-6">
             <div>
-              <label htmlFor="" className={styles.label}>
-                amount
+              <label htmlFor="amount" className={styles.label}>
+                Amount
               </label>
               <input
+                id="amount"
                 name="amount"
                 onChange={handleChange}
                 value={form.amount}
@@ -60,10 +78,11 @@ const Fundedmodal = ({ closeModal }) => {
               />
             </div>
             <div>
-              <label htmlFor="" className={styles.label}>
-                code
+              <label htmlFor="code" className={styles.label}>
+                Code
               </label>
               <input
+                id="code"
                 name="code"
                 onChange={handleChange}
                 value={form.code}
@@ -72,10 +91,11 @@ const Fundedmodal = ({ closeModal }) => {
               />
             </div>
             <div>
-              <label htmlFor="" className={styles.label}>
-                reason
+              <label htmlFor="reason" className={styles.label}>
+                Reason
               </label>
               <textarea
+                id="reason"
                 name="reason"
                 onChange={handleChange}
                 value={form.reason}
@@ -85,16 +105,18 @@ const Fundedmodal = ({ closeModal }) => {
             </div>
             <div className="flex justify-end gap-4">
               <button
+                type="button"
                 onClick={closeModal}
                 className="bg-gray-500 py-2 px-6 text-white rounded-lg"
               >
-                cancel
+                Cancel
               </button>
               <button
+                type="submit"
                 onClick={handleSubmit}
                 className="bg-purple-500 py-2 px-6 text-white rounded-lg"
               >
-                confirm
+                Confirm
               </button>
             </div>
           </form>
